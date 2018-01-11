fa972ec014534563fed7a36262af12bbce553b16
Keegan Westphal <31550979+ramenblack@users.noreply.github.com>
2018-01-11 12:01:18 -0800

    Add files via upload

---
+++ hello2.c
@@ -0,0 +1,12 @@
+// save this file as hello.c
+// compile $gcc hrllo.c -o hello.o
+// to run $./hello.o
+
+
+#include <stdio.h>
+
+     int main(){
+     printf ("hello world");
+
+     return 0;
+}
---
+++ helloworld.c
@@ -0,0 +1,11 @@
+//save file as helloworld.c
+//compile $gcc helloworld.c -o helloworld.o
+
+#include <stdio.h>
+
+int main(){
+
+    printf ("hello world \n\n");
+
+}
+
